487420777a7e8a29d5a16ed02c23b71c13bc3c28
jey <jey@jeys-MacBook-Pro.local>
2020-05-11 19:31:58 +0300

    Add the production build

---
+++ build/precache-manifest.a9dcf2b9af9cf928f246106ee97846f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673a0101ee59b1fe019b957dd1d33e05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6c8704075732844c842",
+    "url": "/static/css/main.1ff9cbe9.chunk.css"
+  },
+  {
+    "revision": "a24ff46e9226b37927bd",
+    "url": "/static/js/2.b9eacea7.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/static/js/2.b9eacea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6c8704075732844c842",
+    "url": "/static/js/main.1ac74d2e.chunk.js"
+  },
+  {
+    "revision": "b8837de9163626b7dc3b",
+    "url": "/static/js/runtime-main.79e1887d.js"
+  }
+]);
